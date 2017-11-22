3e1f4420ed98a89319de7e6e23fe2529783c09e5
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-11-21 18:45:29 -0800

    started page routing

---
+++ ksplants/scripts/controllers/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// page('/', app.mapController.init);
+// page('/about', app.aboutController.init);
+
+
+// continue to work on
+
+
+
+page();
---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+const PORT = process.env.PORT || 4000;
+const bodyParser = require('body-parser');
+const app = express();
+
+// continue to work on
